322f281e7f1a80a80697429260092dc86e65d658
Happinessprashant <125643307+Happinessprashant@users.noreply.github.com>
2023-07-18 08:08:09 +0530

    add multiThreading files

--- MultiThreading/mulitithreding.py
+++ MultiThreading/mulitithreding.py
@@ -1,5 +1,4 @@
 
-
 # multithreading:
 # In Python, multithreading is a technique that allows multiple threads of 
 # execution to run concurrently within a single process. Each thread represents
@@ -7,24 +6,14 @@
 # Multithreading is commonly used to perform multiple tasks concurrently, 
 # improve performance, and enhance the responsiveness of applications.
 
-
 import threading
-
-
 def test(id):
     print("program start %d" %id)
-    
-
 # test(45)    
 
 thread = [threading.Thread(target=test , args=(i ,))for i in range(10)]
-
-
 for i in thread:
     i.start()
-    
-    
-    
+   
 print(id(thread))
-
-print(thread )
+print(thread)
