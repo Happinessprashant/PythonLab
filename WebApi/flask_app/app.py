3198c660ec75015bc5fbf71a46956730fba191c4
Prashantjadhav2620 <125643307+Happinessprashant@users.noreply.github.com>
2023-08-03 07:30:10 +0530

    modify flask file use the postman to test the Api

--- WebApi/flask_app/app.py
+++ WebApi/flask_app/app.py
@@ -6,6 +6,40 @@ app= Flask(__name__)
 def home_page():
     return render_template("index.html")
 
+@app.route("/postman_data", methods=["POST","GET"])
+def math_ops():
+        if request.method=="POST":
+            ops=request.json["operation"]
+            num1=int(request.json['num1'])
+            num2=int(request.json["num2"])
+        
+        if request.method=="GET":
+            ops=request.args.get("operation")
+            num1=int(request.args.get('num1'))
+            num2=int(request.args.get("num2"))
+            
+        
+        if ops =="add":
+            r=num1+num2
+            result= (f" Addition of {num1} and {num2} is {r}")                                                                                                                                                      
+        elif ops =="subtract":
+            r=num1-num2
+            result= (f"Substration of {num1}  and {num2} is {r}")   
+            
+        elif ops =="multiply":
+            r=num1*num2
+            result= (f"Substration of {num1}  and {num2} is {r}")   
+            
+        elif ops =="divide":
+            r=num1/num2
+            result= (f"division of {num1}  and {num2} is {r}")   
+            
+        else:
+            result=(f"{ops} is not arithmetic  operation please check valid operation") 
+            
+        return jsonify(result)  
+    
+     
 @app.route("/math", methods=["POST"])
 def math_ops():
         ops=request.form["operation"]
@@ -15,7 +49,7 @@ def math_ops():
         if ops =="add":
             r=num1+num2
             result= (f" Addition of {num1} and {num2} is {r}")
-        elif ops =="substract":
+        elif ops =="subtract":
             r=num1-num2
             result= (f"Substration of {num1}  and {num2} is {r}")   
             
