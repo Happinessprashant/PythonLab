dbae7ac0b1467f993f9db738a822af661ccce1e9
Prashantjadhav2620 <125643307+Happinessprashant@users.noreply.github.com>
2023-08-07 08:13:48 +0530

    add csv create and use using Pandas library

---
+++ Pandas/csv1.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+#read the csv file
+df=pd.read_csv('services.csv')
+
+# print csv file
+
+print("==========================\n data print\n ==========================\n",df)
+
+
+
+
+
+
+
+
+
+
+
+
