ad090864e2ef5be38f749de465f5bd17732b0bbf
Prashant Shivaji Jadhav <125643307+Prashantjadhav2620@users.noreply.github.com>
2023-10-22 16:21:05 +0530

    22/10/2023

--- hii.py
+++ hii.py
@@ -1,1 +1,18 @@
 print("Hello World")
+
+============================================================
+
+Deeplearning 
+
+Principle 1
+Write clear and specific instructions
+
+Tactic 1: Use delimiters
+    Triple quotes: """""",
+Triple backticks: ™,
+Triple dashes: ---,
+Angle brackets: < >,
+  XML tags: <tag> <[tag>
+                    
+===========================================
+                    
